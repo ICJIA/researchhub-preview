050823000ddef12e1d6240e2cee5893465c56b3a
Bobae Kang <bkang.dev@gmail.com>
2019-08-16 11:38:02 -0500

    fix: adjust spacing & simplify

--- src/components/PreviewLayout.vue
+++ src/components/PreviewLayout.vue
@@ -2,36 +2,26 @@
   <BaseViewLayout :view.sync="view">
     <template>
       <div v-show="view">
-        <div v-if="contentType === 'article'">
-          <slot name="view"></slot>
-        </div>
+        <slot v-if="contentType === 'article'" name="view"></slot>
 
-        <v-row v-else justify="center">
-          <v-col cols="12" sm="10" lg="8" xl="7">
-            <slot name="view"></slot>
-          </v-col>
-        </v-row>
+        <v-col v-else class="mx-auto" cols="12" sm="10" lg="8" xl="7">
+          <slot name="view"></slot>
+        </v-col>
       </div>
 
-      <v-row v-show="!view" justify="center">
-        <v-col cols="12" sm="10" lg="8" xl="7">
-          <v-row v-if="contentType === 'app'" justify="center">
-            <v-col cols="12" sm="6" lg="4">
-              <slot name="card"></slot>
-            </v-col>
-          </v-row>
+      <v-col v-show="!view" class="mx-auto" cols="12" sm="10" lg="8" xl="7">
+        <slot v-if="contentType === 'article'" name="card"></slot>
 
-          <template v-if="contentType === 'article'">
-            <slot name="card"></slot>
-          </template>
-
-          <v-row v-if="contentType === 'dataset'" justify="center">
-            <v-col cols="12" xl="6">
-              <slot name="card"></slot>
-            </v-col>
-          </v-row>
+        <v-col
+          v-else
+          class="mx-auto"
+          cols="12"
+          sm="6"
+          :lg="contentType === 'app' ? 4 : 6"
+        >
+          <slot name="card"></slot>
         </v-col>
-      </v-row>
+      </v-col>
     </template>
   </BaseViewLayout>
 </template>
