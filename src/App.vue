e61bd0af80a3487d93977bd488b850dc515302ac
Bobae Kang <bkang.kr@gmail.com>
2019-08-16 14:23:03 -0500

    chore: bump app version to v0.3.2

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@ export default {
       },
       github: {
         url: 'https://github.com/icjia/icjia-research-preview',
-        version: '0.3.1'
+        version: '0.3.2'
       }
     }
   },
