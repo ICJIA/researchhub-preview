50f8f31e8541ff3e79dfbe37313612237c452cbc
Bobae Kang <bkang.dev@gmail.com>
2019-10-09 10:19:13 -0500

    chore(config): remove irrelevant items

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  agency: {
-    name: 'Illinois Criminal Justice Information Authority',
-    url: 'http://www.icjia.state.il.us/'
-  },
   apiBaseURL:
     process.env.VUE_APP_API_BASE_URL || 'https://research.icjia-api.cloud',
   github: 'https://github.com/icjia/icjia-research-preview',
-  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/preview/',
-  version: '1.0.0-beta.5'
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/preview/'
 }
