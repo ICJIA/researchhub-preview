dcabe4056661143b261de3c9bab0dc68ad04b8f8
Bobae Kang <bkang.dev@gmail.com>
2019-10-11 16:32:20 -0500

    chore(config): update default values as necessary

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiBaseURL:
-    process.env.VUE_APP_API_BASE_URL || 'https://research.icjia-api.cloud',
+    process.env.VUE_APP_API_BASE_URL || 'https://researchhub.icjia-api.cloud',
   github: 'https://github.com/icjia/icjia-research-preview',
   publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/preview/'
 }
