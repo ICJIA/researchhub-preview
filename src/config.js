6759674b249ea710d2dedf139f2c6844688c2422
Bobae Kang <bkang.dev@gmail.com>
2019-10-18 15:36:18 -0500

    chore: add url for upload files

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   apiBaseURL:
     process.env.VUE_APP_API_BASE_URL || 'https://researchhub.icjia-api.cloud',
+  filesBaseURL:
+    process.env.VUE_APP_FILES_BASE_URL ||
+    'https://icjia.illinois.gov/researchhub/files',
   github: 'https://github.com/icjia/icjia-research-preview',
   publicPath: process.env.VUE_APP_PUBLIC_PATH || '/researchhub/preview/'
 }
